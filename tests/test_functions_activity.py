8e8b9ec2388ce26e9238b4a0fa3821a319715fc2
Kobos <61996066+rokobo@users.noreply.github.com>
2023-06-23 10:32:42 -0300

    More tests

--- tests/test_functions_activity.py
+++ tests/test_functions_activity.py
@@ -18,9 +18,44 @@ def test_total_by_category() -> None:
         'total': [0.5],
         'rowid': [1]
     })
+    categories = ['Work', 'Personal', 'Neutral']
     result = total_by_category(dataframe)
     correct = pd.DataFrame({
-        'category': ['Work', 'Personal', 'Neutral'],
-        'total': [0, 0.5, 0]
-    })
+        'category': categories, 'total': [0, 0.5, 0]})
     assert result.equals(correct), (result, correct)
+
+    dataframe.loc[0, 'category'] = "Neutral"
+    dataframe.loc[0, 'total'] = 0.75
+    result = total_by_category(dataframe)
+    correct = pd.DataFrame({
+        'category': categories, 'total': [0, 0, 0.75]})
+    assert result.equals(correct), dataframe
+
+    dataframe.loc[0, 'category'] = "Personal"
+    dataframe.loc[0, 'total'] = 1.2
+    result = total_by_category(dataframe)
+    correct = pd.DataFrame({
+        'category': categories, 'total': [0, 1.2, 0]})
+    assert result.equals(correct), dataframe
+
+    dataframe = pd.DataFrame({
+        'process_name': ['test.exe', 'test2.exe'],
+        'subtitle': ['info', 'info2'],
+        'category': ['Personal', 'Personal'],
+        'method': ['(A)', '(D)'],
+        'duration': ['30 minutes', '1 hour'],
+        'total': [0.5, 1],
+        'rowid': [1, 2]
+    })
+    categories = ['Work', 'Personal', 'Neutral']
+    result = total_by_category(dataframe)
+    correct = pd.DataFrame({
+        'category': categories, 'total': [0, 1.5, 0]})
+    assert result.equals(correct), (result, correct)
+
+    dataframe.loc[0, 'category'] = "Neutral"
+    dataframe.loc[0, 'total'] = 0.75
+    result = total_by_category(dataframe)
+    correct = pd.DataFrame({
+        'category': categories, 'total': [0, 1, 0.75]})
+    assert result.equals(correct), dataframe

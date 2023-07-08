6a2d70201530392b408839e9a9a9c1c51444cd15
Kobos <61996066+rokobo@users.noreply.github.com>
2023-07-08 07:42:30 -0300

    Fix OSError interrupting app

--- src/functions_threads.py
+++ src/functions_threads.py
@@ -2,7 +2,7 @@
 Creates event listeners that will be used to detect if the user
 is not idle. If the user is not idle, update data file.
 """
-# pylint: disable=unused-variable
+# pylint: disable=unused-variable, bare-except
 # flake8: noqa: F401
 import time
 import struct
@@ -62,6 +62,44 @@ def activity_detector():
         time.sleep(cfg['ACTIVITY_CHECK_INTERVAL'])
 
 
+def detect_audio():
+    """Save idle time if audio detected."""
+    with pyaudio.PyAudio() as audio:
+        # Get speaker info
+        wasapi_info = audio.get_host_api_info_by_type(pyaudio.paWASAPI)
+        default_speakers = audio.get_device_info_by_index(
+            wasapi_info["defaultOutputDevice"])
+
+        if not default_speakers["isLoopbackDevice"]:
+            for loopback in audio.get_loopback_device_info_generator():
+                if default_speakers["name"] in loopback["name"]:
+                    default_speakers = loopback
+                    break
+            else:
+                raise OSError("No device, run `python -m pyaudiowpatch`")
+
+        def stream_callback(in_data, _frame_count, _time_info, _status):
+            """Check audio intensity and save input detection if needed."""
+            intensity = max(
+                abs(sample) for sample
+                in struct.unpack(f"<{len(in_data) // 2}h", in_data))
+            if intensity > 1:
+                save_dataframe(
+                    pd.DataFrame({'time': [int(time.time())]}), 'audio')
+                return (in_data, pyaudio.paAbort)
+            return (in_data, pyaudio.paContinue)
+
+        with audio.open(
+            format=pyaudio.paInt24,
+            channels=default_speakers["maxInputChannels"],
+            rate=int(default_speakers["defaultSampleRate"]),
+            frames_per_buffer=pyaudio.get_sample_size(pyaudio.paInt24),
+            input=True,
+            input_device_index=default_speakers["index"],
+            stream_callback=stream_callback
+        ) as stream:
+            time.sleep(1)
+
 def audio_idle_detector():
     """
     Detects if audio had activity. Waits a couple of seconds
@@ -69,41 +107,10 @@ def audio_idle_detector():
     """
     while True:
         cfg = load_config()
-        with pyaudio.PyAudio() as audio:
-            # Get speaker info
-            wasapi_info = audio.get_host_api_info_by_type(pyaudio.paWASAPI)
-            default_speakers = audio.get_device_info_by_index(
-                wasapi_info["defaultOutputDevice"])
-
-            if not default_speakers["isLoopbackDevice"]:
-                for loopback in audio.get_loopback_device_info_generator():
-                    if default_speakers["name"] in loopback["name"]:
-                        default_speakers = loopback
-                        break
-                else:
-                    raise OSError("No device, run `python -m pyaudiowpatch`")
-
-            def stream_callback(in_data, _frame_count, _time_info, _status):
-                """Check audio intensity and save input detection if needed."""
-                intensity = max(
-                    abs(sample) for sample
-                    in struct.unpack(f"<{len(in_data) // 2}h", in_data))
-                if intensity > 1:
-                    save_dataframe(
-                        pd.DataFrame({'time': [int(time.time())]}), 'audio')
-                    return (in_data, pyaudio.paAbort)
-                return (in_data, pyaudio.paContinue)
-
-            with audio.open(
-                format=pyaudio.paInt24,
-                channels=default_speakers["maxInputChannels"],
-                rate=int(default_speakers["defaultSampleRate"]),
-                frames_per_buffer=pyaudio.get_sample_size(pyaudio.paInt24),
-                input=True,
-                input_device_index=default_speakers["index"],
-                stream_callback=stream_callback
-            ) as stream:
-                time.sleep(1)
+        try:
+            detect_audio()
+        except:
+            print("Audio idle detector error")
         time.sleep(cfg['IDLE_CHECK_INTERVAL'])
 
 

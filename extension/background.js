88e6f38d6d559f5d59c3a07fdc10bff538a12fea
Kobos <61996066+rokobo@users.noreply.github.com>
2023-08-22 15:28:06 -0300

    No error pause

--- extension/background.js
+++ extension/background.js
@@ -15,30 +15,24 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 // Gets titles and urls, then sends them through GET request to application
 async function sendURLs()  {
-    var errorStatus = false;
     while (true) {
         await delay(1500);
 
         const { titles, urls } = await getTabsInfo();
         const queryParams = new URLSearchParams({ titles: titles.join('|-|'), urls: urls.join('|-|') }).toString();
         const url = `http://localhost:8050/urldata?${queryParams}`;
-    
-        await fetch(url)
-        .then(function(response) {
-            if (response.status !== 200) {
-                console.log('Error, status Code: ' + response.status);
-                errorStatus = true;
-            }
-        })
-        .catch(error => {
-            errorStatus = true;
-        })
-        if (errorStatus) {
-            errorStatus = false;
-            console.log("Error, waiting 15 seconds to reconnect...");
-            await delay(15000);
+        try {
+            await fetch(url)
+            .then(function(response) {
+                if (response.status !== 200) {
+                    console.log('Error, status Code: ' + response.status);
+                }
+            })
+            console.log("GET request sent")
         }
-        console.log("GET request sent")
+        catch (error) {
+            console.log("Error");
+        }
     }
 }
 
